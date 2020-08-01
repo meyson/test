9598773f8bf3657cb48fcf8ffd31d6230312c896
vova <vovazav172839@gmail.com>
2020-08-01 12:35:31 +0300

    fix

--- test_req.py
+++ test_req.py
@@ -4,9 +4,9 @@ r = requests.post('https://httpbin.org/post', data={'key': 'value'})
 
 print('Response: ')
 print(r.json())
-print('- headers'*60)
+print('-'*60)
 print(r.headers)
-print('- cookies'*60)
+print('-'*60)
 
 print(r.cookies)
 
