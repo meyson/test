ba491def24daacaf0f1fa607706a903412c686a5
vova <vovazav172839@gmail.com>
2020-08-01 12:23:28 +0300

    add cookies

--- test_req.py
+++ test_req.py
@@ -4,5 +4,9 @@ r = requests.post('https://httpbin.org/post', data={'key': 'value'})
 
 print('Response: ')
 print(r.json())
-print('-'*60)
+print('- headers'*60)
 print(r.headers)
+print('- cookies'*60)
+
+print(r.cookies)
+
